55615744370be33c432bbbf34e0e1e21b180e5ed
Englishdud <91146628+Englishdud@users.noreply.github.com>
2022-08-05 12:24:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // number=parseInt(prompt("What is the number you want to cube and square? "))
+for (let i=0; i<5; i++){
     randomnum=9
     console.log('What is you guess 1-10, you have currently have', 5-i , 'guesses remaining')
     guess=parseInt(prompt('So what is your guess? '))

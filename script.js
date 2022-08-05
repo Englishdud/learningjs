10d9518d93516e5f25ba518e81dfb9e40c560a0b
Englishdud <91146628+Englishdud@users.noreply.github.com>
2022-08-05 12:23:56 -0400

    done

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-number=parseInt(prompt("What is the number you want to cube and square? "))
-
-function sac(numbers){
-    answers=numbers * numbers
-    answerc=numbers * numbers * numbers
-    answerc=String(answerc)
-    answers=String(answers)
-    answer=("The Cube Is " + answerc + " And The Square Is " + answers)
-    return answer
+// number=parseInt(prompt("What is the number you want to cube and square? "))
+    randomnum=9
+    console.log('What is you guess 1-10, you have currently have', 5-i , 'guesses remaining')
+    guess=parseInt(prompt('So what is your guess? '))
+    if (guess == randomnum && guess <= 10){
+        console.log('You got it right! ')
+        break
+    }
+    else{
+        console.log('Wrong Answer or Incorrect Response')
+    }
 }
-X=(sac(number))
-console.log(X)
-console.log("Done...")
